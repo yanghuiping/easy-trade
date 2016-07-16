7af434fdc039ecbb81d6d88441ee692b732e062b
yanghuiping <yanghuiping@91xcm.com>
2016-07-16 11:04:27 +0800

    add project

---
+++ src/main/test/com/yc/easy/trade/test/demo/TestReplace.java
@@ -0,0 +1,10 @@
+package com.yc.easy.trade.test.demo;
+
+public class TestReplace {
+
+	public static void main(String[] args) {
+		String abc = "aaa fdsa fdasfa fdas";
+		System.out.println(abc.replaceAll(" ", "+"));
+	}
+
+}
